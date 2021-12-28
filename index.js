549551406907aa43ed0408f078b39180f7685cb3
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2021-12-28 09:39:22 -0300

    pagina picos pronta

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
 
+const botaoLink = document.querySelector('.menu-lateral');
+const link = document.querySelector('.menu-lateral__link')
+
+botaoLink.addEventListener('click', () => {
+    link.classList.toggle('menu-lateral__link--ativo')
+})
